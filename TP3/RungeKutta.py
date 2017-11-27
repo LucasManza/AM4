a9410b3d3be7d88e039c449a0f4d44aa3eef3c98
LucasManza <lucas.manzanelli@ing.austral.edu.ar>
2017-11-27 19:37:26 -0300

    adf

--- TP3/RungeKutta.py
+++ TP3/RungeKutta.py
@@ -1,16 +1,6 @@
-def runge_kutta(x, y, step, final_x, fun):
-    xi = x
-    yi = y
-    while xi < final_x:
-        xi += step
-        yi = runge_kutta_step(xi, yi, step, fun)
-    return yi
-
-
-def runge_kutta_step(x, y, h, fun):
-    k1 = fun(x, y)
-    k2 = fun(x + 1 / 2 * h, y + 1 / 2 * h * k1)
-    k3 = fun(x + 1 / 2 * h, y + 1 / 2 * h * k2)
-    k4 = fun(x + h, y + k3 * h)
-
-    return y + 1/6 * h * (k1 + 2*k2 + 2*k3 + k4)
+def rungeKuttaOrder_4(xi, yi, h, function):
+    k1 = function(xi, yi)
+    k2 = function(xi + (h / 2), yi + (k1 * (h / 2)))
+    k3 = function(xi + (h / 2), yi + (k2 * (h / 2)))
+    k4 = function(xi + h, yi + (k3 * h))
+    return yi + ((h / 6) * (k1 + k2 + k3 + k4))
